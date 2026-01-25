c8ab9eecd691c77ff83e7050628b85b17107c007
jatinverma2007 <wwwjatinverma2007@gmail.com>
2026-01-26 02:35:22 +0530

    frontend deploy

--- MERN-Blog/backend/server.js
+++ MERN-Blog/backend/server.js
@@ -19,8 +19,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials:true
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true
 }))
 
 const _dirname = path.resolve()
